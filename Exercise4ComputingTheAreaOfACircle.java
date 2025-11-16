76840eb2a7eee6cf9fdd4ac7619b059b8b04ee55
Azizrahman rahmani <rahmaniazizrahman9@gmail.com>
2025-11-16 12:25:10 +0430

    Add files via upload

---
+++ Exercise4ComputingTheAreaOfACircle.java
@@ -0,0 +1,13 @@
+
+package exercise4computing.the.area.of.a.circle;
+
+
+public class Exercise4ComputingTheAreaOfACircle {
+    public static void main(String[] args) {
+double radius=20;
+double Area=radius*radius*3.14159;
+        System.out.println("The Area for the Circle of radius"+radius+"is"+Area);
+        
+    }
+    
+}
